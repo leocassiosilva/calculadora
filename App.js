fb32d3978b62afd561940f57ccf426b10f20d659
leocassiosilva <leocassiosilva1234@gmail.com>
2021-07-06 21:45:20 -0300

    first commit

--- App.js
+++ App.js
@@ -5,8 +5,62 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.conteudo}>
+        <Text>Calculadora!</Text>
+        <View style={styles.display}></View>
+        <View style={styles.limpar}></View>
+        <View style={styles.containerBotoes}>
+          <View style={styles.colunaBotoes1}>
+            <View style={styles.botoes}>
+            </View>
+            <View style={styles.botoes}>
+            </View>
+            <View style={styles.botoes}>
+            </View>
+            <View style={styles.botoes}>
+            </View>
+          </View>
+          <View style={styles.colunaBotoes1}>
+          <View style={styles.botoes}>
+            </View>
+            <View style={styles.botoes}>
+            </View>
+            <View style={styles.botoes}>
+            </View>
+            <View style={styles.botoes}>
+            </View>
+          </View>
+          <View style={styles.colunaBotoes1}>
+            <View style={styles.botoes}>
+            </View>
+            <View style={styles.botoes}>
+            </View>
+            <View style={styles.botoes}>
+            </View>
+            <View style={styles.botoes}>
+            </View>
+          </View>
+          <View style={styles.colunaBotoes1}>
+          <View style={styles.botoes}>
+            </View>
+            <View style={styles.botoes}>
+            </View>
+            <View style={styles.botoess}>
+            </View>
+            
+          </View>
+          <View style={styles.colunaBotoes1}>
+          <View style={styles.botoes}>
+            </View>
+            <View style={styles.botoes}>
+            </View>
+            <View style={styles.botoes}>
+            </View>
+            <View style={styles.botoes}>
+            </View>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
@@ -18,4 +72,51 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  conteudo: {
+    backgroundColor: 'red',
+    height:'60%',
+    width:'100%',
+    justifyContent: 'space-evenly',
+
+   }, 
+   display:{
+     backgroundColor:'blue',
+     height:100,
+     margin:10,
+     borderRadius:10
+   }, 
+   limpar:{
+    backgroundColor:'black',
+    height:60,
+    width:60, 
+    alignSelf:'flex-end',
+    borderRadius:10,
+    marginRight:10 
+  }, 
+  containerBotoes:{
+    backgroundColor:'white',
+    height:280,
+    flexDirection:'row',
+    justifyContent: 'space-between',
+    margin:10
+  },
+  colunaBotoes1:{
+    backgroundColor:'blue',
+    height:280,
+    width:60,
+    justifyContent: 'space-evenly',
+  },
+  botoes:{
+    backgroundColor:'black',
+    height:60,
+    width:60,
+    borderRadius:10
+  },
+  botoess:{
+    backgroundColor:'black',
+    height:130,
+    width:60,
+    borderRadius:10
+  }
+
 });
